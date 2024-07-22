96052e70f35f94b175a32602314b9432342eec8d
Tedi Mitiku <tedi.m52@gmail.com>
2024-07-22 12:59:50 -0400

    fix spacing and indentation

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import copy
 
 def create_flow(servicespec, deploymentspec, flow_uuid, dev_branch_host):
     # assume dev branch already exists for now
+
 	# deployment_spec = create_and_replace_branch(deployment_spec)
+
     new_deployment_spec = copy.deepcopy(deployment_spec)
 
     # change neondb proxy to point to dev branch
-	new_deployment_spec['spec']['template']['spec']['containers'][0]['env']['PG_SERVER_NAME'] = dev_branch_host
 
-	return new_deployment_spec, config_map
+    new_deployment_spec['spec']['template']['spec']['containers'][0]['env']['PG_SERVER_NAME'] = dev_branch_host
+
+    return new_deployment_spec, config_map
 	
 def delete_flow(config_map, flow_uuid):
     # delete dev branch when its done
