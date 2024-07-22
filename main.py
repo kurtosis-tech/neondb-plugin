d7a1e4d4f1182dcdf08f1d253ca25ea04d4102c2
Tedi Mitiku <tedi.m52@gmail.com>
2024-07-22 14:13:50 -0400

    only adjust PG_SERVER_HOSTNAME

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import copy
 
 def create_flow(service_spec, deployment_spec, flow_uuid, dev_branch_host, NEON_PROJECT_ID, NEON_MAIN_BRANCH_ID, NEON_API_KEY):
-    # assume dev branch already exists for now
-    new_deployment_spec = copy.deepcopy(deployment_spec)
+    assume script exists for now
+    modified_deployment_spec = copy.deepcopy(deployment_spec)
 
-    # change neondb proxy to point to dev branch
+    container = modified_deployment_spec['spec']['template']['spec']['containers'][0]
 
-    print(new_deployment_spec)
-    new_deployment_spec['spec']['template']['spec']['containers'][0]['env']['PG_SERVER_NAME'] = dev_branch_host
-    new_deployment_spec['spec']['template']['spec']['containers'][0]['env']['PG_SERVER_NAME'] = dev_branch_host
-    new_deployment_spec['spec']['template']['spec']['containers'][0]['env']['PG_SERVER_NAME'] = dev_branch_host
-    new_deployment_spec['spec']['template']['spec']['containers'][0]['env']['PG_SERVER_NAME'] = dev_branch_host
+    container['env'] = container.get('env', []) + [
+        {'name': 'PG_SERVER_HOSTNAME', 'value': dev_branch_host},
+        # {'name': 'NEON_API_KEY', 'value': db_user},
+        # {'name': 'NEON_MAIN_BRANCH_ID', 'value': db_password},
+        # {'name': 'NEON_PROJECT_ID', 'value': db_password},
+    ]
+
+    modified_deployment_spec['spec']['template']['spec']['containers'] = [container]
 
     return {
-        "deployment_spec": new_deployment_spec, 
-        "config_map": config_map,
+        "deployment_spec": modified_deployment_spec,
+        "config_map": {}
     }
 	
 def delete_flow(config_map, flow_uuid):
     # delete dev branch when its done
 	# neon_api, neon_branch, neon_project_id = extract_neon_data(config_map, service_name)
 	# neon.delete_branch(neon_api, neon_branch, neon_project_id)
-
     return
 
 def create_and_replace_branch(deployment_spec):
