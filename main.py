2df2319ec57997fe1afbca33f09d7f6b7b2e6367
Tedi Mitiku <tedi.m52@gmail.com>
2024-07-22 13:24:36 -0400

    rename kwargs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import copy
 
-def create_flow(servicespec, deploymentspec, flow_uuid, dev_branch_host):
+def create_flow(service_spec, deployment_spec, flow_uuid, dev_branch_host, neon_project_id, neon_main_branch_id, neon_api_key):
     # assume dev branch already exists for now
 
 	# deployment_spec = create_and_replace_branch(deployment_spec)
@@ -14,10 +14,10 @@ def create_flow(servicespec, deploymentspec, flow_uuid, dev_branch_host):
     return new_deployment_spec, config_map
 	
 def delete_flow(config_map, flow_uuid):
+    return
     # delete dev branch when its done
 	# neon_api, neon_branch, neon_project_id = extract_neon_data(config_map, service_name)
 	# neon.delete_branch(neon_api, neon_branch, neon_project_id)
-    return
 
 def create_and_replace_branch(deployment_spec):
     return
