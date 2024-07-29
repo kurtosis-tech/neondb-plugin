53dc633bf8f977a0602a8614912acb67e3ae53f4
Tedi Mitiku <tedi.m52@gmail.com>
2024-07-29 12:48:06 -0400

    add logic to create branch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import copy
+import requests
+import json
 
 def create_flow(service_spec, deployment_spec, flow_uuid, dev_branch_host, NEON_PROJECT_ID, NEON_MAIN_BRANCH_ID, NEON_API_KEY):
     # assume script exists for now
@@ -6,11 +8,13 @@ def create_flow(service_spec, deployment_spec, flow_uuid, dev_branch_host, NEON_
 
     container = modified_deployment_spec['template']['spec']['containers'][0]
 
+    dev_branch_host = create_neon_branch(NEON_API_KEY, NEON_PROJECT_ID, NEON_MAIN_BRANCH_ID)
+    if error:
+        # how are errors handled in plugins
+        print(f"Error: {error}")
+
     container['env'] = [
         {'name': 'PG_SERVER_HOSTNAME', 'value': dev_branch_host},
-        # {'name': 'NEON_API_KEY', 'value': db_user},
-        # {'name': 'NEON_MAIN_BRANCH_ID', 'value': db_password},
-        # {'name': 'NEON_PROJECT_ID', 'value': db_password},
     ]
 
     modified_deployment_spec['template']['spec']['containers'] = [container]
@@ -26,8 +30,46 @@ def delete_flow(config_map, flow_uuid):
 	# neon.delete_branch(neon_api, neon_branch, neon_project_id)
     return
 
-def create_and_replace_branch(deployment_spec):
+def create_neon_branch(neon_api_key, project_id, parent_branch_id):
+    url = f"https://console.neon.tech/api/v2/projects/{project_id}/branches"
+
+    json_payload = {
+        "endpoints": [{"type": "read_write"}],
+        "branch": {"parent_id": parent_branch_id}
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {neon_api_key}"
+    }
+
+    try:
+        response = requests.post(url, json=json_payload, headers=headers)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        return "", f"Error sending request: {e}"
+
+    if response.status_code != 201:
+        return "", f"Unexpected status code: {response.status_code}, body: {response.text}"
+
+    try:
+        result = response.json()
+    except json.JSONDecodeError as e:
+        return "", f"Error decoding response: {e}"
+
+    endpoints = result.get("endpoints", [])
+    if not endpoints:
+        return "", "Endpoints not found in response"
+
+    first_endpoint = endpoints[0]
+    host = first_endpoint.get("host")
+    if not host:
+        return "", "Host not found in response"
+
+    return host, None
+
+def delete_neon_branch(config_map, service_name):
     return
 
-def extract_neon_data(config_map, service_name):
+def extract_neon_data():
     return
