2944603ab62ae497b7649b1673e0451f666cf5f0
Tedi Mitiku <tedi.m52@gmail.com>
2024-08-02 10:16:27 -0400

    update plugin to operate on the dependent service

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
+from urllib.parse import urlparse, urlunparse
 import copy
 import requests
 import json
 
 def create_flow(service_spec, deployment_spec, flow_uuid, NEON_PROJECT_ID, NEON_MAIN_BRANCH_ID, NEON_API_KEY):
-    # assume script exists for now
     modified_deployment_spec = copy.deepcopy(deployment_spec)
 
     container = modified_deployment_spec['template']['spec']['containers'][0]
 
-    dev_branch_host, error = create_neon_branch(NEON_API_KEY, NEON_PROJECT_ID, NEON_MAIN_BRANCH_ID)
+    postgres_url = ""
+    for env in container['env']:
+        if env.get("name") == "POSTGRES":
+            postgres_url = env.get("value")
+
+    dev_branch_hostname, error = create_neon_branch(NEON_API_KEY, NEON_PROJECT_ID, NEON_MAIN_BRANCH_ID)
     if error:
-        # how are errors handled in plugins?
         print(f"Error: {error}")
 
+    new_postgres_url = update_postgres_url(postgres_url, dev_branch_hostname)
     container['env'] = [
-        {'name': 'PG_SERVER_HOSTNAME', 'value': dev_branch_host},
+        {'name': 'POSTGRES', 'value': new_postgres_url},
     ]
 
     modified_deployment_spec['template']['spec']['containers'] = [container]
 
     return {
         "deployment_spec": modified_deployment_spec,
-        "config_map": {}
+        "config_map": {
+            "NEON_PROJECT_ID": NEON_PROJECT_ID,
+            "NEON_MAIN_BRANCH_ID": NEON_MAIN_BRANCH_ID,
+            "NEON_API_KEY": NEON_API_KEY:
+        }
     }
 	
 def delete_flow(config_map, flow_uuid):
@@ -68,8 +77,14 @@ def create_neon_branch(neon_api_key, project_id, parent_branch_id):
 
     return host, None
 
+def update_postgres_url(postgres_url, new_hostname):
+    parsed_url = urlparse(postgres_url)
+    new_netloc = f"{new_hostname}:{parsed_url.port}" if parsed_url.port else new_hostname
+    updated_url = urlunparse(parsed_url._replace(netloc=new_netloc))
+    return updated_url
+
 def delete_neon_branch(config_map, service_name):
     return
 
-def extract_neon_data():
+def extract_neon_data(config_map, service_name):
     return
